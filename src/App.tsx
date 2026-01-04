1bfc5351f5664d6034206199a0b81e98758ac843
Yann <yannngolamze@gmail.com>
2026-01-04 03:08:19 +0100

    initialisation_repo

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div data-theme="dark">
+      <div className='font-google text-textcol bg-primary  dark:bg-primary-dark dark:text-textcol-dark p-4'>
+        Test reussi
+      </div>
+      YO YO c'est un test
+    </div>
+      
+    </>
+  )
+}
+
+export default App
